16520280b592c353935e04910b141c9fae241a87
LuisBonesGarCanga <149491081+LuisBonesGarCanga@users.noreply.github.com>
2024-02-27 19:52:30 +0000

    A

---
+++ TIC-II/js-ejerc01-primer_programa.js
@@ -0,0 +1,61 @@
+alert("Esto lo ejecuto desde un archivo externo!")
+alert("Esto lo ejecuto desde un archivo externo!");
+alert("Este es un mensaje interno");
+alert("Y este es otro mensaje externo");
+alert("Paso 1: Calienta una sartén a fuego medio");
+alert("Paso 2: Agrega un poco de aceite a la sartén");
+alert("Paso 3: Rompe el huevo y viértelo en la sartén");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//¿Estás leyendo estó?..................
+//¿qué haces leyendo mi código rufián?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*.Tot el camp
+és un clam
+som la gent blaugrana.
+Tant se val d’on venim
+si del sud o del nord
+ara estem d’acord, estem d’acord,
+una bandera ens agermana.
+Blaugrana al vent
+un crit valent
+tenim un nom
+el sap tothom:
+Barça, Barça, Barça!*/
+
+
+
+
+
+
+
+
+
+
